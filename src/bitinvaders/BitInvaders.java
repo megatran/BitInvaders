aee8f140efbab19dc1798b82a956eeaa1e71a1c0
Karen Chen <kchen@mymail.mines.edu>
2016-04-30 10:16:38 -0600

    moved locatrion of the score and pause, cursor begins in boz

--- src/bitinvaders/BitInvaders.java
+++ src/bitinvaders/BitInvaders.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -22,28 +23,28 @@ public class BitInvaders extends JFrame implements Commons {
 	private static JTextField lifeDisplay;
 	private static boolean hasShownDialogue = false;
 	private static boolean gameOver = false;
+	private JTextField input;
+	private JTextField scoreOutput;
 
-	public BitInvaders()
-    {
+	public BitInvaders(){
 		board = new Board();
-		//setLayout(new GridLayout(0,1,0,3));
         add(board);
         setTitle("Bit Invaders");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(createBottomPanel(), BorderLayout.SOUTH);
+        add(makeScore(), BorderLayout.NORTH);
         setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         pack();
         //setLocationRelativeTo(null);
         setVisible(true);
+        input.requestFocus();
         setResizable(false);
         
         JMenuBar bar = new JMenuBar();
 		setJMenuBar(bar);
 		JMenu file = new JMenu("File");
 		bar.add(file);
-		JMenuItem pause = new JMenuItem("Pause");
 		JMenuItem exit = new JMenuItem("Exit");
-		file.add(pause);
 		file.add(exit);
 		class ExitListener implements ActionListener
 		{
@@ -52,6 +53,24 @@ public class BitInvaders extends JFrame implements Commons {
 			}
 		}
 		exit.addActionListener(new ExitListener());
+		
+    }
+
+	private JPanel createBottomPanel() {
+		JPanel panelBottom = new JPanel();
+		JTextField userInput = new JTextField(3);
+		input = userInput;
+		JLabel lifeLabel = new JLabel("Life");
+		JButton pause = new JButton("Pause");
+		lifeDisplay = new JTextField(2);
+		lifeDisplay.setEditable(false);
+		lifeDisplay.setText(Integer.toString(board.player.getNumLives()));
+		panelBottom.setSize(BOARD_WIDTH, 10);
+		panelBottom.add(userInput);
+		panelBottom.add(lifeLabel);
+		panelBottom.add(lifeDisplay);
+		panelBottom.add(pause);
+		
 		class PauseListener implements ActionListener
 		{
 			public void actionPerformed(ActionEvent e) {
@@ -62,31 +81,10 @@ public class BitInvaders extends JFrame implements Commons {
 				else {
 					pause.setText("Pause");
 					board.unpause();
-				}
-				
+				}	
 			}
 		}
 		pause.addActionListener(new PauseListener());
-    }
-
-	private JPanel createBottomPanel() {
-		JPanel panelBottom = new JPanel();
-		JTextField userInput = new JTextField(5);
-		JLabel scoreLabel = new JLabel("Score");
-		JLabel lifeLabel = new JLabel("Life");
-		JTextField scoreOutput = new JTextField(5);
-		scoreOutput.setText(Integer.toString(0));
-		lifeDisplay = new JTextField(5);
-		lifeDisplay.setEditable(false);
-		lifeDisplay.setText(Integer.toString(board.player.getNumLives()));
-		scoreOutput.setEditable(false);
-		panelBottom.setSize(BOARD_WIDTH, 10);
-		panelBottom.add(scoreLabel);
-		panelBottom.add(scoreOutput);
-		panelBottom.add(userInput);
-		panelBottom.add(lifeLabel);
-		panelBottom.add(lifeDisplay);
-		
 		//userInput textfield only accept numeric keypress by user DocumentFilter to check!
 		((AbstractDocument)userInput.getDocument()).setDocumentFilter(new DigitDocumentFilter());
 		//the user input will accept numeric input from the user when he/she presses 'Enter'
@@ -101,7 +99,17 @@ public class BitInvaders extends JFrame implements Commons {
 		
 		return panelBottom;
 	}
-	
+	private JPanel makeScore(){
+		JPanel scorePanel = new JPanel();
+		JLabel scoreLabel = new JLabel("Score");
+		scorePanel.add(scoreLabel);
+		JTextField scoreOutput = new JTextField(5);
+		this.scoreOutput=scoreOutput;
+		scoreOutput.setText(Integer.toString(0));
+		scoreOutput.setEditable(false);
+		scorePanel.add(scoreOutput);
+		return scorePanel;
+	}
 	public static void displayLife(){
 		lifeDisplay.setText(Integer.toString(board.getPlayer().getNumLives()));
 	}
